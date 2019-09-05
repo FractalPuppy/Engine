19c30715f8ca68aadf44226323b8f59184c39628
RogerNogue <rogernogueb@gmail.com>
2019-09-05 15:12:26 +0200

    made some checkings of data before doing the navmesh query

--- Source/ModuleNavigation.cpp
+++ Source/ModuleNavigation.cpp
@@ -1662,7 +1662,12 @@ bool ModuleNavigation::IsCursorPointingToNavigableZone(float xPickingCorrection,
 	float polyPickExt[3] = { xPickingCorrection, yPickingCorrection, zPickingCorrection };
 
 	//nav mesh query call
-	navQuery->findNearestPoly((float*)& intersectionPos, polyPickExt, &filter, &resultPoly, 0); // find closest poly
+	//first check we got nav mesh and stuff
+	if (navMesh && navQuery)
+	{
+		navQuery->findNearestPoly((float*)& intersectionPos, polyPickExt, &filter, &resultPoly, 0); // find closest poly
+	}
+	
 
 	return resultPoly;
 }
