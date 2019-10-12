41332644f3be440e1b409bfbacb119bba15a5de3
Rubén Crispín De la Cruz <ruben.crispin@hotmail.com>
2019-10-12 17:33:41 +0200

    Decreased critical damage

--- Scripts/EnemyControllerScript/EnemyControllerScript.cpp
+++ Scripts/EnemyControllerScript/EnemyControllerScript.cpp
@@ -30,6 +30,7 @@
 
 #define MINIMUM_PATH_DISTANCE 400.0f
 #define MOVE_REFRESH_TIME 0.3f
+#define CRITICAL_DAMAGE 1.2f
 
 EnemyControllerScript_API Script* CreateScript()
 {
@@ -452,7 +453,7 @@ inline math::float3 EnemyControllerScript::GetPlayerPosition() const
 inline void EnemyControllerScript::SetPosition(math::float3 newPos) const
 {
 	assert(gameobject->transform != nullptr);
-	gameobject->transform->SetGlobalPosition(newPos);
+gameobject->transform->SetGlobalPosition(newPos);
 }
 
 inline float EnemyControllerScript::GetDistanceTo(math::float3& position) const
@@ -550,17 +551,20 @@ void EnemyControllerScript::OnTriggerEnter(GameObject* go)
 			// Get base damage
 			int totalDamage = playerMovement->stats.strength;
 
-			// Generate a random number and if it is below the critical chance -> double base damage
-			bool critical = (rand() % 100u) < playerMovement->criticalChance;	
-			if (critical)
-				totalDamage *= 2;
-
 			// Add damage of the skill
 			PlayerSkill* skill = playerMovement->GetSkillInUse();
 			if (skill != nullptr)
 				totalDamage *= skill->damage;
 
-			TakeDamage(totalDamage, critical ? (int)DamageType::CRITICAL : (int)DamageType::NORMAL);
+			// Generate a random number and if it is below the critical chance -> increase damage
+			if ((rand() % 100u) < playerMovement->criticalChance)
+			{
+				TakeDamage(totalDamage * CRITICAL_DAMAGE, (int)DamageType::CRITICAL);
+			}
+			else
+			{
+				TakeDamage(totalDamage, (int)DamageType::NORMAL);
+			}
 		}
 	}
 }
