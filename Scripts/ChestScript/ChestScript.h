ac6f240b914255933b150706a672998b48c9ae0a
sondy99@gmail.com <sondy99@gmail.com>
2019-10-13 15:53:57 +0200

    Solving error with cursor when hover items and UI

--- Scripts/ChestScript/ChestScript.h
+++ Scripts/ChestScript/ChestScript.h
@@ -61,6 +61,7 @@ private:
 	float chestTimer = 0.0f;										
 	float lootDelay = 2.5f;										// Time since chest is opened untill loot is spawned
 	float lootRadius = 100.0f;									// Distance from enemy position to drop Items around (only if Items to drop > 1)
+	bool changeItemCursorIcon = true;
 };
 
 extern "C" ChestScript_API Script* CreateScript();
