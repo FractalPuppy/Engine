7677b04988ba77e49bc474a67488f7926ae27d5c
AlbertVVila <albert.val.vila@est.fib.upc.edu>
2019-11-03 22:49:35 +0100

    Buried enemy sound

--- Scripts/BuriedEnemyAIScript/EnemyStateChase.cpp
+++ Scripts/BuriedEnemyAIScript/EnemyStateChase.cpp
@@ -2,6 +2,9 @@
 
 #include "BuriedEnemyAIScript.h"
 #include "EnemyControllerScript.h"
+#include "Application.h"
+#include "ModuleTime.h"
+#include "ComponentAudioSource.h"
 
 EnemyStateChase::EnemyStateChase(BuriedEnemyAIScript* AIScript)
 {
@@ -40,6 +43,22 @@ void EnemyStateChase::HandleIA()
 
 void EnemyStateChase::Update()
 {
+	timer += enemy->App->time->gameDeltaTime;
+	if (timer >= walkTimer && enemy->audioFoot != nullptr)
+	{
+		timer = 0.0f;
+		enemy->audioFoot->Play();
+		float offset = enemy->randomOffset(0.4) - 0.2;
+		enemy->audioFoot->SetPitch(1.0 + offset);
+	}
 	// Move towards the player
 	enemy->enemyController->Move(enemy->runSpeed, refreshTime, enemy->enemyController->GetPlayerPosition(), enemyPath);
+}
+
+void EnemyStateChase::Exit()
+{
+	if (enemy->audioFoot != nullptr)
+	{
+		enemy->audioFoot->Stop();
+	}
 }
