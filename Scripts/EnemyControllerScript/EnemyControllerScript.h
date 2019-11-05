23a059735118fcf0dc467997e12fd11e2ff9c1fb
Rubén Crispín <ruben.crispin@hotmail.com>
2019-11-05 16:59:00 +0100

    Added missing reference

--- Scripts/EnemyControllerScript/EnemyControllerScript.h
+++ Scripts/EnemyControllerScript/EnemyControllerScript.h
@@ -14,6 +14,7 @@
 class ComponentAnimation;
 class ComponentRenderer;
 class ComponentBoxTrigger;
+class ComponentAudioSource;
 class DamageController;
 class EnemyLifeBarController;
 class PlayerMovement;
